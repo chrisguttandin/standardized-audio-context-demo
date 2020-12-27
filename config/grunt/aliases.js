2c6052fb297cf108ba27ddd11e60b47c4ddf5b70
Christoph Guttandin <chrisguttandin@media-codings.com>
2020-12-27 03:17:03 +0100

    build(config): run postcss with its cli

--- config/grunt/aliases.js
+++ config/grunt/aliases.js
@@ -6,11 +6,11 @@ const isVersionUpdate =
     env.TRAVIS === 'true' && env.TRAVIS_PULL_REQUEST === 'false' && env.TRAVIS_SECURE_ENV_VARS === 'true' && env.TRAVIS_TAG !== '';
 
 module.exports = {
-    'build:development': ['clean', 'htmlmin', 'webpack:development', 'postcss:development'],
-    'build:production': ['clean', 'htmlmin', 'webpack:production', 'postcss:production'],
+    'build:development': ['clean', 'htmlmin', 'webpack:development', 'sh:build-development'],
+    'build:production': ['clean', 'htmlmin', 'webpack:production', 'sh:build-production'],
     'deploy': ['build:production', 'gh-pages:deploy'],
     'deploy-on-version-updates': filter([isVersionUpdate ? 'deploy' : null]),
-    'lint': ['postcss:lint', 'sh:lint-config', 'sh:lint-src'],
+    'lint': ['sh:lint-config', 'sh:lint-src'],
     'monitor': ['build:development', 'connect', 'watch:development'],
     'preview': ['build:production', 'connect', 'watch:production'],
     'smoke': ['sh:hyperlink']
