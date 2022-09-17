cd0c6d6dac1754c3bd2493b532402d9130174391
Christoph Guttandin <chrisguttandin@media-codings.com>
2022-09-17 22:52:17 +0200

    build(config): update grunt config

--- config/grunt/aliases.js
+++ config/grunt/aliases.js
@@ -1,7 +1,6 @@
 module.exports = {
     'build:development': ['clean', 'htmlmin', 'sh:build-development'],
     'build:production': ['clean', 'htmlmin', 'sh:build-production'],
-    'deploy': ['build:production', 'gh-pages:deploy'],
     'lint': ['sh:lint-config', 'sh:lint-src'],
     'monitor': ['build:development', 'connect', 'watch:development'],
     'preview': ['build:production', 'connect', 'watch:production'],
